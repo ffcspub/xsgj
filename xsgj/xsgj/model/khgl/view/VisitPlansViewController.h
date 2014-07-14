d60ba3e463fb90ace06492356596b07eaef8d480
ilikeido <ilikeido@163.com>
2014-07-14 22:27:03 +0800

    更新目录结构

---
+++ xsgj/xsgj/model/khgl/view/VisitPlansViewController.h
@@ -0,0 +1,13 @@
+//
+//  VisitPlansViewController.h
+//  xsgj
+//
+//  Created by ilikeido on 14-7-14.
+//  Copyright (c) 2014年 ilikeido. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VisitPlansViewController : UIViewController
+
+@end
---
+++ xsgj/xsgj/model/khgl/view/VisitPlansViewController.m
@@ -0,0 +1,38 @@
+//
+//  VisitPlansViewController.m
+//  xsgj
+//
+//  Created by ilikeido on 14-7-14.
+//  Copyright (c) 2014年 ilikeido. All rights reserved.
+//
+
+#import "VisitPlansViewController.h"
+
+@interface VisitPlansViewController ()
+
+@end
+
+@implementation VisitPlansViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
