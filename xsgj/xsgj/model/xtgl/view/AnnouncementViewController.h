14fae644f245ee14df9d73a16aebc406c4e0bafa
Geory <1003491960@qq.com>
2014-07-26 00:46:12 +0800

    企业公告

---
+++ xsgj/xsgj/model/xtgl/view/AnnouncementViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnnouncementViewController.h
+//  xsgj
+//
+//  Created by Geory on 14-7-25.
+//  Copyright (c) 2014年 ilikeido. All rights reserved.
+//
+
+#import "HideTabViewController.h"
+
+@interface AnnouncementViewController : HideTabViewController
+
+@end
---
+++ xsgj/xsgj/model/xtgl/view/AnnouncementViewController.m
@@ -0,0 +1,38 @@
+//
+//  AnnouncementViewController.m
+//  xsgj
+//
+//  Created by Geory on 14-7-25.
+//  Copyright (c) 2014年 ilikeido. All rights reserved.
+//
+
+#import "AnnouncementViewController.h"
+
+@interface AnnouncementViewController ()
+
+@end
+
+@implementation AnnouncementViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
