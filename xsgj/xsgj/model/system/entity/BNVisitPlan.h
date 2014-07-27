75d689c9f866796071ab56229fc2022ee28fc179
ilikeido <ilikeido@163.com>
2014-07-27 22:16:17 +0800

    no message

--- xsgj/xsgj/model/system/entity/BNVisitPlan.h
+++ xsgj/xsgj/model/system/entity/BNVisitPlan.h
@@ -12,4 +12,8 @@
 @property (nonatomic,assign)    int WEEKDAY;
 @property (nonatomic,assign)    int CUST_ID;
 @property (nonatomic,assign)    int ORDER_NO;
+@property (nonatomic,assign)    int LINE_ID;//	线路id	Long
+@property (nonatomic,assign)    int CORP_ID;//	企业id	Long
+@property (nonatomic,assign)    int CHECK_STATE;//	审核状态0:未审核1:通过2:未通过3:申请删除	Integer
+@property (nonatomic,assign)    int USER_ID;//	用户id	long
 @end
