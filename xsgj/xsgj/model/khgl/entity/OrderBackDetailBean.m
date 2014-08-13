e80e45b3a88b40926a302980e03a9114c6f17e51
jackyzonewen <jackyzonewen@gmail.com>
2014-08-13 10:25:22 +0800

    [Fix] : 错误修改

--- xsgj/xsgj/model/khgl/entity/OrderBackDetailBean.m
+++ xsgj/xsgj/model/khgl/entity/OrderBackDetailBean.m
@@ -7,21 +7,7 @@
 //
 
 #import "OrderBackDetailBean.h"
-#import <LKDBHelper.h>
-#import <NSDate+Helper.h>
 
 @implementation OrderBackDetailBean
-
-//表名
-+(NSString *)getTableName
-{
-    return [NSString stringWithFormat:@"t_%d_OrderBackDetailBean",[ShareValue shareInstance].userInfo.USER_ID];
-}
-
--(void)save
-{
-    [OrderBackDetailBean deleteWithWhere:[NSString stringWithFormat:@"ITEM_ID=%d",_ITEM_ID]];
-    [self saveToDB];
-}
 
 @end
